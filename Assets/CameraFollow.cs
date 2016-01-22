6a9a003c19de0aff5cba7b591e2250ed832829b6
Luzer59 <luzer59@hotmail.com>
2016-01-22 09:16:28 +0200

    Toimiva proto xD

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        if (target)
+        {
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10f);
+        }
+    }
+}
--- Assets/LevelController.cs
+++ Assets/LevelController.cs
@@ -7,13 +7,17 @@ public class LevelController : MonoBehaviour
     public GameObject[] levelPrefabsVertical;
     public GameObject levelPrefabTreasureRoom;
     public int treasureRoomPlace = 5;
+    public KeyCode newLevelKey;
+    public GameObject playerPrefab;
 
     private bool firstLevelReady = false;
     private GameObject lastInitializedLevel;
     private Vector3 lastEndpoint;
     private Vector3 newStartpoint;
+    public Vector3 nextEndpointWorldSpace;
     private GameObject newLevel;
     private int currentLevel = 0;
+    private GameObject player;
 
     public enum NextLevelStyle { Horizontal, Vertical, TreasureRoom }
     private NextLevelStyle levelStyle = NextLevelStyle.Horizontal;
@@ -21,6 +25,7 @@ public class LevelController : MonoBehaviour
     void Start()
     {
         RandomizeLevel();
+        CreatePlayer();
     }
 
     void RandomizeLevel()
@@ -58,6 +63,8 @@ public class LevelController : MonoBehaviour
             newLevel.transform.position = lastInitializedLevel.transform.position + lastEndpoint - newStartpoint;
             lastInitializedLevel = newLevel;   
         }
+        nextEndpointWorldSpace = lastInitializedLevel.transform.Find("Endpoint").position;
+
         if (currentLevel == treasureRoomPlace)
         {
             levelStyle = NextLevelStyle.TreasureRoom;
@@ -69,12 +76,25 @@ public class LevelController : MonoBehaviour
         currentLevel++;
     }
 
+    void CreatePlayer()
+    {
+        player = (GameObject)Instantiate(playerPrefab, lastInitializedLevel.transform.Find("Startpoint").position, Quaternion.identity);
+        Camera.main.GetComponent<CameraFollow>().target = player;
+    }
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(newLevelKey))
         {
             RandomizeLevel();
         }
+        if (player)
+        {
+            if (Vector3.Distance(player.transform.position, nextEndpointWorldSpace) < 1f)
+            {
+                RandomizeLevel();
+            }
+        } 
     }
 }
