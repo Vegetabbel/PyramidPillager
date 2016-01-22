6660d776a476de41241115526e826e1359fab00c
Vegetabbel <veikkahuttunen@kamk.fi>
2016-01-22 11:42:18 +0200

    Movement lähes valmis
    Ei aina pysty hyppäämään (ei detectaa maata)
    Vaatii hienosäätöä jossain vaiheessa

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,20 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
     private Rigidbody rb;
+
     private float moveSpeedPhysics; //'Active' move speed, set in Update()
     private float groundMoveSpeedPhysics = 100f;
     private float airMoveSpeedPhysics; //Is set in Start()
-    private float sprintMoveSpeedPhysics = 123; //Is added on top of normal move speed
-    private float sprintMaxMoveSpeedPhysics = 12f;
-    private float maxMoveSpeedPhysics = 8f;
+    private float sprintMoveSpeedPhysics = 100f; //Is added on top of normal move speed
+    private float maxMoveSpeedPhysics; //'Active max move speed, set in Update() and Start()
+    private float normalMaxMoveSpeedPhysics = 8f;
+    private float sprintMaxMoveSpeedPhysics = 16f;
+
     private float moveSpeedArcade = 10f;
-    private float jumpForce = 400f;
-    private float wallJumpForce = 300f;
+
+    private float jumpForce = 120f;
+    private float wallJumpForce = 200f;
+
     private bool isGrounded = false;
     private bool isTouchingLeftWall = false;
     private bool isTouchingRightWall = false;
@@ -24,11 +29,28 @@ public class PlayerMovement : MonoBehaviour {
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
-        airMoveSpeedPhysics = groundMoveSpeedPhysics * 0.5f;
+        airMoveSpeedPhysics = groundMoveSpeedPhysics * 0.2f;
+        maxMoveSpeedPhysics = normalMaxMoveSpeedPhysics;
 	}
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift) || !isGrounded)
+        {
+            maxMoveSpeedPhysics = sprintMaxMoveSpeedPhysics;
+        }
+        else
+        {
+            maxMoveSpeedPhysics = normalMaxMoveSpeedPhysics;
+        }
+        if (Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        {
+            moveSpeedPhysics = sprintMoveSpeedPhysics;
+        }
+        else
+        {
+            moveSpeedPhysics = normalMaxMoveSpeedPhysics;
+        }
         if (!isGrounded)
         {
             moveSpeedPhysics = airMoveSpeedPhysics;
@@ -40,14 +62,14 @@ public class PlayerMovement : MonoBehaviour {
             moveSpeedArcade = 10f;
         }
 
-        if (Physics.Raycast(gameObject.transform.position, -Vector3.up, 1f))
+        /*if (Physics.Raycast(gameObject.transform.position, -Vector3.up, 1f))
         {
             isGrounded = true;
         }
         else
         {
             isGrounded = false;
-        }
+        }*/
     }
 
     void FixedUpdate ()
@@ -55,19 +77,15 @@ public class PlayerMovement : MonoBehaviour {
         switch (controlState)
         {
             case Controls.Physics:
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    
-                }
                 //Move left
                 if (Input.GetKey(KeyCode.A))
                 {
-                    rb.AddForce(-1 * groundMoveSpeedPhysics, 0, 0);
+                    rb.AddForce(-1 * moveSpeedPhysics, 0, 0);
                 }
                 //Move right
                 if (Input.GetKey(KeyCode.D))
                 {
-                    rb.AddForce(groundMoveSpeedPhysics, 0, 0);
+                    rb.AddForce(moveSpeedPhysics, 0, 0);
                 }
                 //Jump
                 if (isGrounded)
@@ -83,20 +101,20 @@ public class PlayerMovement : MonoBehaviour {
                     {
                         if (Input.GetKey(KeyCode.Space))
                         {
-                            rb.AddForce(wallJumpForce, jumpForce * 0.5f, 0);
+                            rb.AddForce(wallJumpForce, jumpForce * 0.3f, 0);
                         }
                     }
                     if (isTouchingRightWall)
                     {
                         if (Input.GetKey(KeyCode.Space))
                         {
-                            rb.AddForce(-1 * wallJumpForce, jumpForce * 0.5f, 0);
+                            rb.AddForce(-1 * wallJumpForce, jumpForce * 0.3f, 0);
                         }
                     }
                 } 
                 rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -maxMoveSpeedPhysics, maxMoveSpeedPhysics), rb.velocity.y, rb.velocity.z);
                 break;
-
+                 
             case Controls.Arcade:
                 //Move left
                 if (Input.GetKey(KeyCode.A))
