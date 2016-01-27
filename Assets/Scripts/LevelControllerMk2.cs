f6448d311d2f0c6a282319e0c95bbe28d2354c05
Vegetabbel <veikkahuttunen@kamk.fi>
2016-01-27 17:56:01 +0200

    Toimiikhan

--- Assets/Scripts/LevelControllerMk2.cs
+++ Assets/Scripts/LevelControllerMk2.cs
@@ -16,6 +16,7 @@ public class LevelControllerMk2 : MonoBehaviour
 {
     /// <summary>
     /// pasdasdasdasdasd
+    /// lol :D
     /// </summary>
     public GameObject playerPrefab;
     public LevelPrefabs levelPrefabs;
@@ -40,7 +41,9 @@ public class LevelControllerMk2 : MonoBehaviour
     {
         if (!firstLevelReady)
         {
-            currentLevel = (GameObject)Instantiate(levelPrefabs.levelPrefabsLeftEntry[Random.Range(0, levelPrefabs.levelPrefabsLeftEntry.Length)], Vector3.zero, Quaternion.identity);
+            currentLevel =  (GameObject)Instantiate(levelPrefabs.levelPrefabsLeftEntry
+                            [Random.Range(0, levelPrefabs.levelPrefabsLeftEntry.Length)],
+                            Vector3.zero, Quaternion.identity);
             levelExitDirection = currentLevel.GetComponent<ExitLocation>().levelExitDirection;
             firstLevelReady = true;
         }
@@ -55,28 +58,36 @@ public class LevelControllerMk2 : MonoBehaviour
             {
                 case LevelExitdirection.left:
                     currentExitpoint = currentLevel.transform.Find("Entrypoints/Left").localPosition;
-                    newLevel = (GameObject)Instantiate(levelPrefabs.levelPrefabsRightEntry[Random.Range(0, levelPrefabs.levelPrefabsRightEntry.Length)], Vector3.zero, Quaternion.identity);
+                    newLevel =  (GameObject)Instantiate(levelPrefabs.levelPrefabsRightEntry
+                                [Random.Range(0, levelPrefabs.levelPrefabsRightEntry.Length)],
+                                Vector3.zero, Quaternion.identity);
                     newLevel.GetComponent<ExitLocation>().levelEntryDirection = LevelExitdirection.right;
                     newStartpoint = newLevel.transform.Find("Entrypoints/Right").localPosition;
                     break;
 
                 case LevelExitdirection.top:
                     currentExitpoint = currentLevel.transform.Find("Entrypoints/Top").localPosition;
-                    newLevel = (GameObject)Instantiate(levelPrefabs.levelPrefabsBottomEntry[Random.Range(0, levelPrefabs.levelPrefabsBottomEntry.Length)], Vector3.zero, Quaternion.identity);
+                    newLevel =  (GameObject)Instantiate(levelPrefabs.levelPrefabsBottomEntry
+                                [Random.Range(0, levelPrefabs.levelPrefabsBottomEntry.Length)],
+                                Vector3.zero, Quaternion.identity);
                     newLevel.GetComponent<ExitLocation>().levelEntryDirection = LevelExitdirection.bottom;
                     newStartpoint = newLevel.transform.Find("Entrypoints/Bottom").localPosition;
                     break;
 
                 case LevelExitdirection.right:
                     currentExitpoint = currentLevel.transform.Find("Entrypoints/Right").localPosition;
-                    newLevel = (GameObject)Instantiate(levelPrefabs.levelPrefabsLeftEntry[Random.Range(0, levelPrefabs.levelPrefabsLeftEntry.Length)], Vector3.zero, Quaternion.identity);
+                    newLevel =  (GameObject)Instantiate(levelPrefabs.levelPrefabsLeftEntry
+                                [Random.Range(0, levelPrefabs.levelPrefabsLeftEntry.Length)],
+                                Vector3.zero, Quaternion.identity);
                     newLevel.GetComponent<ExitLocation>().levelEntryDirection = LevelExitdirection.left;
                     newStartpoint = newLevel.transform.Find("Entrypoints/Left").localPosition;
                     break;
 
                 case LevelExitdirection.bottom:
                     currentExitpoint = currentLevel.transform.Find("Entrypoints/Bottom").localPosition;
-                    newLevel = (GameObject)Instantiate(levelPrefabs.levelPrefabsTopEntry[Random.Range(0, levelPrefabs.levelPrefabsTopEntry.Length)], Vector3.zero, Quaternion.identity);
+                    newLevel =  (GameObject)Instantiate(levelPrefabs.levelPrefabsTopEntry
+                                [Random.Range(0, levelPrefabs.levelPrefabsTopEntry.Length)],
+                                Vector3.zero, Quaternion.identity);
                     newLevel.GetComponent<ExitLocation>().levelEntryDirection = LevelExitdirection.top;
                     newStartpoint = newLevel.transform.Find("Entrypoints/Top").localPosition;
                     break;
@@ -89,20 +100,6 @@ public class LevelControllerMk2 : MonoBehaviour
             previousLevel = currentLevel;
             currentLevel = newLevel;
         }
-        /*nextEndpointWorldSpace = currentLevel.transform.Find("Endpoint").position;
-        newStartpointWorldSpace = currentLevel.transform.Find("Startpoint").position;
-        blocker = currentLevel.transform.Find("Model/Door").gameObject;
-        blocker.SetActive(false);
-
-        if (currentLevel == treasureRoomPlace)
-        {
-            levelStyle = NextLevelStyle.TreasureRoom;
-        }
-        else if (currentLevel == treasureRoomPlace + 1)
-        {
-            levelStyle = NextLevelStyle.Vertical;
-        }
-        currentLevel++;*/
     }
 
     void CreatePlayer()
