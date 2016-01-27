ae05e94c8a1486d16e31c90ecb4928f05f759dbc
Jere Isopahkala <tubekarva@gmail.com>
2016-01-27 16:40:37 +0200

    Highlighting
    same

---
+++ Assets/Scripts/HoverHighlightScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverHighlightScript : MonoBehaviour {
+
+	public Sprite sprite1;
+	public Sprite sprite2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter () {
+		GetComponent <SpriteRenderer>().sprite = sprite1;
+	}
+
+	void OnMouseExit() {
+		GetComponent <SpriteRenderer>().sprite = sprite2;
+	}
+}
